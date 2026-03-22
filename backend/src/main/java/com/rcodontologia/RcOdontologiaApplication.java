047a4bf9331d8408b4250b1922695cef6a2cb82e
Gabriel Rdorigues dos Santos <gabrielrsantos999@gmail.com>
2026-03-22 20:45:55 -0300

    first commit

---
+++ backend/src/main/java/com/rcodontologia/RcOdontologiaApplication.java
@@ -0,0 +1,13 @@
+package com.rcodontologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcOdontologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcOdontologiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rcodontologia/RcOdontologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcodontologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcOdontologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
